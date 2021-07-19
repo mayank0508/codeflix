e6786be79cd875eff13449236074f0fbf255f10f
mayank0508 <thakurmayank0508@gmail.com>
2021-07-20 02:36:50 +0530

    Header files declared 👨‍💻

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GlobalStyle } from './GlobalStyle'; // these are the styles that will be rendered
 
 // const Star = () => React.createElement('div', null, 'This is a little star'); // this is a alternate to JSX
 
@@ -6,9 +7,9 @@ const App = () => {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />      {/* // this is how we declare the global style in the document */}
     </div>
   );
-}
+};
 
 export default App;
- 
