7be5facc604aeec7cf1a8300e03d6f63480e9af5
mayank0508 <thakurmayank0508@gmail.com>
2021-07-21 02:36:36 +0530

    home components started 📺

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+//Config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'; // these are the default congiuration values
+//Component
+
+//hooks
+
+//Images
+import NoImg from '../images/no_image.jpg';
+
+const Home = () => {
+  const [state, useState] = useState();
+
+  return <div>Home page</div>;
+};
