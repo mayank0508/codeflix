2d5667a756005d9302ddf5c352261974e88bbfeb
mayank0508 <thakurmayank0508@gmail.com>
2021-07-24 13:16:50 +0530

    new movies arrya added along with old arrays

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const fetchMovies = async (page, searchTerm = "") => {
+  const fetchMovies = async (page, searchTerm = '') => {
     // here we have defined a function called
     //fetchMovies using async function which has
     // 2 arguments called page and seachTerm, that
@@ -26,19 +26,31 @@ const Home = () => {
       setLoading(true);
 
       const movies = await apiSettings.fetchMovies(searchTerm, page);
-      console.log(movies);
       // here we have used the await method to fetch the movies from the API
+
+      setState(prev => ({
+        // this prev will give us the movies, and when
+        //we call the new movies then it will become available old moviess
+
+        ...movies, // here we have used it this way to make it re render, which is very imp
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results] // this is a ternary operation that i have wrote to
+        // render the new movies in the array without removing the old ones.
+      }));
     } catch (error) {
       setError(true);
     } // here we are using the catch method to catch the error
+
+    setLoading(false); // now since everything has loaded 👍
   };
 
-//Intitial Render
-useEffect(() => {
-fetchMovies(1)
-}, []) // here the [] is empty that means that the function will only render once
+  //Intitial Render
+  useEffect(() => {
+    fetchMovies(1);
+  }, []); // here the [] is empty that means that the function will only render once
 
-  
+console.log(state);
+
   return <div>Home page</div>;
 };
 
