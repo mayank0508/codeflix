f96e5f4430cd792ef5329f9785a5f798553f69e2
mayank0508 <thakurmayank0508@gmail.com>
2021-08-07 02:47:22 +0530

    Routing started

---
+++ src/components/Movies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Movies = () => {
+  <div>Movies</div>;
+};
+
+export default Movies;
---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Notfound = () => {
+    <div></div>
+}
+
+export default Notfound;
