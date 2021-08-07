88df8b0f675cc3ff1ba6c6fb6839a335fd5985d2
mayank0508 <thakurmayank0508@gmail.com>
2021-08-07 14:07:39 +0530

    Movies router done 👨‍💻

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import RMDBLogo from '../../images/react-movie-logo.svg';
 import TMDBLogo from '../../images/tmdb_logo.svg';
-import { Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles';
-
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from './Header.styles';
 
 const Header = () => (
-<Wrapper>  {/* here the work of the wrapper is to just create a card */}
-  <Content> {/* here the work of the content is to make a place where we can add logos */}
-    <LogoImg src={RMDBLogo} alt="rmdb-logo"/>  {/* here we actually we declare the main logo */}
-    <TMDBLogoImg src={TMDBLogo} alt="tmdb_logo" />  {/* here we actually we declare the tmdb logo */}
+  <Wrapper>
+    {' '}
+    {/* here the work of the wrapper is to just create a card */}
+    <Content>
+      {/* here the work of the content is to make a place where we can add logos */}
+      <Link to="/">
+        {' '}
+        {/* here the link to helps us in making the react movie logo a kind of link which traces us back to the home page*/}
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />{' '}
+        {/* here we actually we declare the main logo */}
+      </Link>{' '}
+      <TMDBLogoImg src={TMDBLogo} alt="tmdb_logo" />{' '}
+      {/* here we actually we declare the tmdb logo */}
     </Content>
-</Wrapper>
+  </Wrapper>
 );
 
 export default Header;
--- src/components/Thumb/Thumb.js
+++ src/components/Thumb/Thumb.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 // styles
 import { Image } from './Thumb.styles';
 
 const Thumb = ({ image, movieId, clickable }) => (
   // we make a implicit return ie () => () here cause here we are only interested to return JSx
   <div>
-    <Image src={image} alt="" />  {/* here the image is a prop passed in the Thumb function */}
+    {clickable ? ( // here it says that if clickable then we should go to Link
+      <Link to={`/${movieId}`}>
+        {/* here it says that if clickable then we should go to movieId link */}
+        <Image src={image} alt="" />
+      </Link>
+    ) : (
+      // if not clickable then
+      <Image src={image} alt="" />
+      /* here the image is a prop passed in the Thumb function */
+    )}
+    ;
   </div>
 );
 
