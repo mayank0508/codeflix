ee23e9d0783a45dfa3bf59a258213d352469358b
mayank0508 <thakurmayank0508@gmail.com>
2021-08-09 01:40:22 +0530

    MovieInfo Text and Content styling done 👾

--- src/components/MovieInfo/MovieInfo.styles.js
+++ src/components/MovieInfo/MovieInfo.styles.js
@@ -7,21 +7,69 @@ export const Wrapper = styled.div`
     { backdrop } // here {({ backdrop: })} this is a prop which is being destructured
   ) =>
     backdrop ? `url(${IMAGE_BASE_URL}${BACKDROP_SIZE}${backdrop})` : '#0000'};
-    background-size: cover;
-    background-position: center;
-    padding: 40px 20px;
-    animation: animateMovieInfo 1s;
+  background-size: cover;
+  background-position: center;
+  padding: 40px 20px;
+  animation: animateMovieInfo 1s;
 
-    @keyframes animateMovieInfo {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
+  @keyframes animateMovieInfo {
+    from {
+      opacity: 0;
     }
+    to {
+      opacity: 1;
+    }
+  }
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  background: rgba(0, 0, 0, 0.8);
+  border-radius: 20px;
 
-export const Text = styled.div``;
+  @media screen and (max-width: 768px) {
+    display: block;
+    max-height: none;
+  }
+`;
+
+export const Text = styled.div`
+  width: 100%;
+  padding: 20px 40px;
+  color: var(--white);
+  overflow: hidden;
+
+  .rating-directors {
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .score {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 35px;
+    height: 35px;
+    background: #fff;
+    color: black;
+    font-weight: 800;
+    border-radius: 50%;
+    margin: 0;
+  }
+
+  .director {
+    margin: 0 0 0 40px;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  h1 {
+    @media screen and (max-width: 768px) {
+      font-size: var(--fontBig);
+    }
+  }
+`;
